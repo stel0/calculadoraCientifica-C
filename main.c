45a369f7eb4034cb61c4906e9b6293d8f4e02457
iantorresm <iantorressm@gmail.com>
2023-11-16 20:16:57 -0300

    menú sin errores de ciclo infinito para opción tipo cadena

--- main.c
+++ main.c
@@ -2,37 +2,44 @@
 #include "stack/pila.h"
 
 int main(){
-  char expresion_infija[SIZE - 1];
-  int op = 0;
-  *expresion_infija = '\0';//vaciar residuos en memoria
+    char expresion_infija[SIZE];
+    int op = 0;
+    *expresion_infija = '\0';//inicializar la cadena como vacía
 
-  while (op != 3)
-  {
+    while (op != 3) {
+        if (strlen(expresion_infija) != 0) {
+            printf("Expresión: %s\n", expresion_infija);
+        }
 
-    if (strlen(expresion_infija) != 0)
-      printf("Expresión: %s\n", expresion_infija);
+        menu();
+        if (scanf("%d", &op) != 1 || op < 1 || op > 3) {
+            printf("Opción inválida. Por favor, elige una opción válida (1-3).\n");
+            while (getchar() != '\n'); // Limpiar el búfer del teclado
+            continue;
+        }
 
-    menu();
-    scanf("%d", &op);
+        switch(op){
+            case 1:
+                printf("Ingrese la expresión infija: ");
+                scanf("%s", expresion_infija); // Tener en cuenta el posible desbordamiento
+                // Controlar sintaxis válida de la expresión
+                break;
+            case 2:
+                // Realiza la conversión y cálculo si se ha ingresado una expresión correcta
+                if (strlen(expresion_infija) != 0) {
+                    char *expresion_postfija = convertir_a_postfija(expresion_infija);
+                    calcular(expresion_postfija);
+                } else {
+                    printf("No se ha ingresado ninguna expresión.\n");
+                }
+                break;
+            case 3:
+                exit(0);
+            default:
+                printf("Opción inválida\n");
+                break;
+        }
+    }
 
-    switch(op){
-      case 1:
-        scanf("%s", expresion_infija);
-        //controlar que la sintaxis sea válida
-        break;
-      case 2:
-        char * expresion_postfija = convertir_a_postfija(expresion_infija);
-        calcular(expresion_postfija);
-        break;
-      case 3:
-        exit(0);
-      default:
-        printf("Opción inválida\n");
-        op = 0;
-        break;
-    }//fin switch
-
-  }//fin while
-
-  return 0;
-}//fin main
+    return 0;
+}
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #define SIZE 256
 
 void menu();
